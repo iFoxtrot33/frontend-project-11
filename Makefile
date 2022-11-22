0f8f899edd4eeaffdeaa8bc2916f339a9aa11f29
iurii <furmaniurii@gmail.com>
2022-11-22 14:33:26 +0400

    Set webpack

--- Makefile
+++ Makefile
@@ -3,9 +3,3 @@ install:
 
 lint:
 	npx eslint .
-
-test:
-	npm test
-
-test-coverage:
-	npm test -- --coverage --coverageProvider=v8
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,49 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
-
-const config = {
-  entry: './src/index.js',
+module.exports = {
+  entry: './src/js/main.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'main.html',
+    }),
+  ],
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    open: true,
-    host: 'localhost',
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
   module: {
     rules: [
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  // eslint-disable-next-line global-require
+                  require('autoprefixer'),
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-  } else {
-    config.mode = 'development';
-  }
-  return config;
 };
