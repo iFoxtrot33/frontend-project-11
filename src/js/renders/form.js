bc0071a7457eaa092849bfef350984e914790202
iurii <furmaniurii@gmail.com>
2022-12-18 11:49:56 +0400

    Set form render

---
+++ src/js/renders/form.js
@@ -0,0 +1,15 @@
+const renderManagementForm = (elements, state) => {
+  switch (state) {
+    case 'edit':
+      elements.containerSubmit.removeAttribute('disabled');
+      elements.input.removeAttribute('disabled');
+      return;
+    case 'send':
+      elements.containerSubmit.setAttribute('disabled', 'true');
+      elements.input.setAttribute('disabled', 'true');
+      return;
+    default:
+      throw Error(`Unknown state: ${state}`);
+  }
+};
+export default renderManagementForm;
